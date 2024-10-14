b6aa03e56fcb8bb80bee4dcc2e03bbe9cc0a750a
Anderson Lozano <adslozanov@gmail.com>
2024-10-14 16:01:30 -0500

    Connection to the database is tested

--- src/main/java/com/eafit/nodo/Main.java
+++ src/main/java/com/eafit/nodo/Main.java
@@ -1,7 +1,25 @@
 package com.eafit.nodo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Cargar las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // Establecer la conexión a la base de datos utilizando try-with-resources
+        try (Connection connection = DriverManager.getConnection(
+                dotenv.get("DB_URL"),
+                dotenv.get("DB_USER"),
+                dotenv.get("DB_PASSWORD")
+        )) {
+            // Imprimir un mensaje si la conexión es exitosa
+            System.out.println("Connection to Database established");
+
+        } catch (SQLException e) {
+            // Imprimir el mensaje de error si ocurre una excepción
+            System.out.println(e.getMessage());
+        }
     }
 }
