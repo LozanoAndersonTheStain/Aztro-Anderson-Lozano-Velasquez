ee9fc3c0a27f536ecbe77b0aa05b3a93ee862add
Anderson Lozano <adslozanov@gmail.com>
2024-10-18 17:14:43 -0500

    Entities are added to create employees and others

--- src/main/java/com/eafit/nodo/config/RepositoryConfig.java
+++ src/main/java/com/eafit/nodo/config/RepositoryConfig.java
@@ -1,18 +1,30 @@
 package com.eafit.nodo.config;
 
+import com.eafit.nodo.models.empleado.Departamento;
+import com.eafit.nodo.models.empleado.Empleado;
+import com.eafit.nodo.models.empleado.Pago;
+import com.eafit.nodo.models.empleado.Proyecto;
 import com.eafit.nodo.models.supermercado.Cliente;
 import com.eafit.nodo.models.supermercado.Compra;
 import com.eafit.nodo.models.supermercado.Marca;
 import com.eafit.nodo.models.supermercado.Producto;
-import com.eafit.nodo.repositories.GenericRepository;
+import com.eafit.nodo.repositories.generics.GenericRepository;
 
 public class RepositoryConfig {
+    //SuperMercado
     public static final GenericRepository<Cliente> clienteRepository;
     public static final GenericRepository<Compra> compraRepository;
     public static final GenericRepository<Marca> marcaRepository;
     public static final GenericRepository<Producto> productoRepository;
 
+    //Empleado
+    public static final GenericRepository<Departamento> departamentoRepository;
+    public static final GenericRepository<Empleado> empleadoRepository;
+    public static final GenericRepository<Pago> pagoRepository;
+    public static final GenericRepository<Proyecto> proyectoRepository;
+
     static {
+        //SuperMercado
         JPAConfig<Cliente> jpaConfigCliente = new JPAConfig<>();
         clienteRepository = new GenericRepository<>(jpaConfigCliente);
 
@@ -24,5 +36,18 @@ public class RepositoryConfig {
 
         JPAConfig<Producto> jpaConfigProducto = new JPAConfig<>();
         productoRepository = new GenericRepository<>(jpaConfigProducto);
+
+        //Empleado
+        JPAConfig<Departamento> jpaConfigDepartamento = new JPAConfig<>();
+        departamentoRepository = new GenericRepository<>(jpaConfigDepartamento);
+
+        JPAConfig<Empleado> jpaConfigEmpleado = new JPAConfig<>();
+        empleadoRepository = new GenericRepository<>(jpaConfigEmpleado);
+
+        JPAConfig<Pago> jpaConfigPago = new JPAConfig<>();
+        pagoRepository = new GenericRepository<>(jpaConfigPago);
+
+        JPAConfig<Proyecto> jpaConfigProyecto = new JPAConfig<>();
+        proyectoRepository = new GenericRepository<>(jpaConfigProyecto);
     }
 }
